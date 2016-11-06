9d243f139716bbd59a4fb14b0629ad8962c0042c
alabarge <adamplabarge@gmail.com>
2016-11-06 10:11:48 -0800

    add Google maps API key

--- wp-content/themes/mbv/functions.php
+++ wp-content/themes/mbv/functions.php
@@ -33,7 +33,13 @@ require_once('functions/custom-functions.php');
 
 require_once('functions/zemplate-functions.php');
 
-//* hot fix to turn off opacify filter on hero images, added directly to functions file on production
+//* ACF Google Maps Field API Key
+add_filter('acf/settings/google_api_key', function () {
+    return 'AIzaSyAkexjpqjbFbW8lrZm7SPH4m8DsPTQk6Mo';
+});
+
+//* turn off opacify filter on hero images, added directly to functions file on production
+//* this needs to be done in SCSS - this is only temp fix
 add_action('wp_head','mtbv_style_hotfix');
 function mtbv_style_hotfix() {
 ?>
