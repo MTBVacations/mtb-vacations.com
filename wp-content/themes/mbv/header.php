68b9705b50a8577a0a6d766deaff4e1da2291e65
Kelly Bleck <kellydbleck@gmail.com>
2017-05-27 13:29:27 -0600

    Sync github master with live master

--- wp-content/themes/mbv/header.php
+++ wp-content/themes/mbv/header.php
@@ -26,6 +26,18 @@
     
     <?php get_template_part('templates/parts/header', 'analytics'); ?>
     <?php get_template_part('templates/parts/header', 'doubleclick'); ?>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-73066983-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 </head>
 
 <body <?php body_class('page-'.$post->post_name); ?>>
