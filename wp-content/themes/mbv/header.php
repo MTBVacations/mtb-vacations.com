72a966bb6ea3d78c0ebcce171b2c85a94a9bad6e
Kelly Bleck <kellydbleck@gmail.com>
2017-07-13 07:31:32 -0600

    Update Analytics/tag manager code

--- wp-content/themes/mbv/header.php
+++ wp-content/themes/mbv/header.php
@@ -24,19 +24,8 @@
 
     <?php wp_head(); //mandatory ?>
     
-    <?php get_template_part('templates/parts/header', 'analytics'); ?>
+    <?php get_template_part('templates/parts/header', 'tag-manager'); ?>
     <?php get_template_part('templates/parts/header', 'doubleclick'); ?>
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-73066983-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 
 </head>
 
--- wp-content/themes/mbv/templates/parts/header-analytics.php
+++ wp-content/themes/mbv/templates/parts/header-analytics.php
@@ -5,52 +5,21 @@ $hostname = $_SERVER['HTTP_HOST']; //dev.zenman.com | localhost | Live server | 
 $remote_addr = $_SERVER['REMOTE_ADDR']; //remote browser ip
 $exclude_ip_range = array('173.164.136.221','69.15.186.249','127.0.0.1');
 
-switch ($hostname) {
-    case 'localhost': //do nothing
-        echo '<!-- no google analytics code -->';
-        break;
-    case (strstr($hostname, '8888') == true): //do nothing
-        echo '<!-- no google analytics code -->';
-        break;
-    case 'dev1.zenman.com': //do nothing
-        echo '<!-- no google analytics code -->';
-        break;
-    case 'test1.zenman.com': //do nothing
-        echo '<!-- no google analytics code -->';
-        break;
-    case 'stage1.zenman.com': //mimic live environment...
-        if(isset($remote_addr) && false == in_array($remote_addr, $exclude_ip_range)){ ?>
-            <script>
-              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+switch ($hostname) { 
+  case 'mtbv.com': // Live site ?>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-              ga('create', 'UA-65539110-1', 'auto');
-              ga('send', 'pageview');
+      ga('create', 'UA-73066983-1', 'auto');
+      ga('send', 'pageview');
 
-            </script>
-        <?php }else{ ?>
-            <!-- No GA code for Zenman! -->
-        <?php
-        }
-        break;
-    default: //this is what you get on live server
-        if(isset($remote_addr) && false == in_array($remote_addr, $exclude_ip_range)){ ?>
-            <script>
-              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-              ga('create', 'UA-65539110-1', 'auto');
-              ga('send', 'pageview');
-
-            </script>
-        <?php }else{ ?>
-            <!-- No GA code for Zenman! -->
-        <?php
-        }
-        break;
+    </script> <?php
+    break;
+  default:
+    // Everything else do nothing
+    break;
 }
 ?>
