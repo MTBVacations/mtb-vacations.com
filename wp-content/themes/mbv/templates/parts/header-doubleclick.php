569f0ad1705ea9eaee29e095ccc5cff1b9d9450f
Kelly Bleck <kellydbleck@gmail.com>
2017-07-17 19:14:22 -0600

    Add switch statement to GA codes

--- wp-content/themes/mbv/templates/parts/header-doubleclick.php
+++ wp-content/themes/mbv/templates/parts/header-doubleclick.php
@@ -1,15 +1,30 @@
-<!-- Enable DoubleClick for Publishers -->
-<script type='text/javascript'>
-  var googletag = googletag || {};
-  googletag.cmd = googletag.cmd || [];
-  (function() {
-    var gads = document.createElement('script');
-    gads.async = true;
-    gads.type = 'text/javascript';
-    var useSSL = 'https:' == document.location.protocol;
-    gads.src = (useSSL ? 'https:' : 'http:') +
-      '//www.googletagservices.com/tag/js/gpt.js';
-    var node = document.getElementsByTagName('script')[0];
-    node.parentNode.insertBefore(gads, node);
-  })();
-</script>
+<?php 
+//Descript: Hostname based google code, only run if on live site. Place right BEFORE </head>
+$hostname = $_SERVER['HTTP_HOST']; //dev.zenman.com | localhost | Live server | etc..
+$remote_addr = $_SERVER['REMOTE_ADDR']; //remote browser ip
+$exclude_ip_range = array('173.164.136.221','69.15.186.249','127.0.0.1');
+
+switch ($hostname) { 
+  case 'mtbv.com': // Live site ?>
+    <!-- Enable DoubleClick for Publishers -->
+    <script type='text/javascript'>
+      var googletag = googletag || {};
+      googletag.cmd = googletag.cmd || [];
+      (function() {
+        var gads = document.createElement('script');
+        gads.async = true;
+        gads.type = 'text/javascript';
+        var useSSL = 'https:' == document.location.protocol;
+        gads.src = (useSSL ? 'https:' : 'http:') +
+          '//www.googletagservices.com/tag/js/gpt.js';
+        var node = document.getElementsByTagName('script')[0];
+        node.parentNode.insertBefore(gads, node);
+      })();
+    </script> <?php
+    break;
+  default:
+    // Everything else do nothing
+    break;
+}
+?>
+
--- wp-content/themes/mbv/templates/parts/header-tag-manager.php
+++ wp-content/themes/mbv/templates/parts/header-tag-manager.php
@@ -1,7 +1,23 @@
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T6JWM69');</script>
-<!-- End Google Tag Manager -->
+<?php 
+//Usage: get_template_part('templates/parts/header', 'analytics');
+//Descript: Hostname based google code, only run if on live site. Place right BEFORE </head>
+$hostname = $_SERVER['HTTP_HOST']; //dev.zenman.com | localhost | Live server | etc..
+$remote_addr = $_SERVER['REMOTE_ADDR']; //remote browser ip
+$exclude_ip_range = array('173.164.136.221','69.15.186.249','127.0.0.1');
+
+switch ($hostname) { 
+  case 'mtbv.com': // Live site ?>
+    <!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-T6JWM69');</script>
+	<!-- End Google Tag Manager -->
+ <?php
+    break;
+  default:
+    // Everything else do nothing
+    break;
+}
+?>
